335796a054af24388ede0edd539bd831b143c47e
duyd <zhouwei@segimail.com>
2024-03-15 16:39:38 +0800

    spring工具类学习

--- demo-utils/src/main/java/com/dyd/strings/StringTest1.java
+++ demo-utils/src/main/java/com/dyd/strings/StringTest1.java
@@ -1,6 +1,9 @@
 package com.dyd.strings;
 
+import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang3.StringUtils;
+
+import java.util.Map;
 
 /**
  * @author ：duyd@segimail.com
@@ -14,5 +17,8 @@ public class StringTest1 {
     public static void main(String[] args) {
         String s = StringUtils.joinWith(",", "22", "33,44");
         System.out.println(s);
+        String ksis = "";
+        Map map = JSONObject.parseObject(ksis, Map.class);
+        System.out.println(map);
     }
 }
